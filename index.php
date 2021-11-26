acc929ad1993f869ed4238ee35af7eb229cd10f0
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-26 14:51:45 +0100

    esercizio php completato

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ $pippoLength= strlen($pippo);
     <title>PHP BadWords</title>
 </head>
 <body>
-    <h1><?php echo $pippo ?></h1>
-    <h1>Il titolo è lungo: <?php echo $pippoLength ?> caratteri</h1>
     <h1>La parola segreta inserita è: <?php echo $censored ?></h1>
+    <h1><?php echo $pippo ?></h1>
+    <h1><?php echo $censored ?> Il titolo è lungo: <?php echo $pippoLength ?> caratteri</h1>
 </body>
 </html>
