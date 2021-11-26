c8ac13b5eec52bf7f2721a5707175d9521130f20
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-26 14:48:46 +0100

    esercizio completato

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+$userWord= $_GET["nome"];
+$censored= str_replace( $userWord, "***" , $userWord);
+$pippo= "Benvenuto" . " " . $censored . " " . "nel primo esercizio su PHP";
+$pippoLength= strlen($pippo);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BadWords</title>
+</head>
+<body>
+    <h1><?php echo $pippo ?></h1>
+    <h1>Il titolo è lungo: <?php echo $pippoLength ?> caratteri</h1>
+    <h1>La parola segreta inserita è: <?php echo $censored ?></h1>
+</body>
+</html>
